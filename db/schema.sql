b3683a0e4c03a5f75262366d3662f83367d3c11c
HartC2019 <hartc94@gmail.com>
2026-07-13 18:42:20 -0600

    Create orders table

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,17 @@
+DROP TABLE IF EXISTS orders;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL
+);
+
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    date DATE NOT NULL,
+    note TEXT,
+    user_id INTEGER NOT NULL
+        REFERENCES users(id)
+        ON DELETE CASCADE
 );
